990ebd40fc3f61a474c0df68dc9e7104ee863ef5
Pietro <pietrospitella@gmail.com>
2021-09-06 18:58:44 +0200

    ✅ ok

---
+++ bonus/js/main.js
@@ -0,0 +1,11 @@
+// BONUS: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue(
+    {
+        el: "#root",
+        data: {
+            titolo: "Hello Vue",
+            img: "img"
+        }
+    }
+);
